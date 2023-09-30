c51e118831616b83821b0751acda00e60a498292
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-30 17:36:15 +0900

    change init.sh

--- sql/init.sh
+++ sql/init.sh
@@ -22,7 +22,7 @@ mysql -u"$ISUCON_DB_USER" \
 
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
+		--host "$ISUCON_DB_HOST_1" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < 7_create_index.sql
 
