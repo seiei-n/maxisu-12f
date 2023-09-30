7d946aca71608482ee6747fa9528d164b4bd6365
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-30 17:54:27 +0900

    fix create_index

--- sql/7_create_index.sql
+++ sql/7_create_index.sql
@@ -1,5 +1,5 @@
 create index idx_login_bonus_id on user_login_bonuses (login_bonus_id);
 create index idx_login_bonus_id on login_bonus_reward_masters (login_bonus_id);
-create index idx_display_order_id on gacha_item_masters (display_order);
+create index idx_display_order_id on gacha_masters (display_order);
 create index idx_created_at_id on user_presents (created_at);
 create index idx_session_id on user_sessions (session_id);
