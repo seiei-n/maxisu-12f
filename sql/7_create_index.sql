7b40844c9b26eb372e02967c7516c9db14d4795d
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-30 18:16:19 +0900

    remove index

--- sql/7_create_index.sql
+++ sql/7_create_index.sql
@@ -1,3 +1,1 @@
 create index idx_display_order_id on gacha_masters (display_order);
-create index idx_created_at_id on user_presents (created_at);
-create index idx_session_id on user_sessions (session_id);
