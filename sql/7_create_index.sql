c8a18235e1343c04dece84472695602fa77f2383
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-30 19:05:34 +0900

    remove waste

--- sql/7_create_index.sql
+++ sql/7_create_index.sql
@@ -2,3 +2,4 @@ create index idx_login_bonus_id on user_login_bonuses (login_bonus_id);
 create index idx_login_bonus_id on login_bonus_reward_masters (login_bonus_id);
 create index idx_display_order_id on gacha_masters (display_order);
 create index idx_session_id on user_sessions (session_id);
+create index idx_item_id on user_items (item_id);
